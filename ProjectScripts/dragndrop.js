9fb1361afbf4899672a44134844e29796e5f2c08
cheyenneo3103 <78625383+cheyenneo3103@users.noreply.github.com>
2021-10-13 00:13:28 -0400

    Add files via upload

--- ProjectScripts/dragndrop.js
+++ ProjectScripts/dragndrop.js
@@ -12,8 +12,61 @@ function allowDiscard(ev) {
   var data = ev.dataTransfer.getData("text");
   ev.target.appendChild(document.getElementById(data));
   xyz.push(data);
+  let cardName = data;
+
+  if(cardName === "1 of Clubs") {    
+    cardName = "Ace Of Clubs";
+  }
+  else if(cardName == "1 of Diamonds") {
+    cardName = "Ace of Diamonds";
+  }
+  else if(cardName == "1 of Hearts") {
+    cardName = "Ace of Hearts";
+  }
+  else if(cardName == "1 of Spades") {
+    cardName = "Ace of Spades";
+  }
+
+  if(cardName === "11 of Clubs") {    
+    cardName = "Jack Of Clubs";
+  }
+  else if(cardName == "11 of Diamonds") {
+    cardName = "Jack of Diamonds";
+  }
+  else if(cardName == "11 of Hearts") {
+    cardName = "Jack of Hearts";
+  }
+  else if(cardName == "11 of Spades") {
+    cardName = "Jack of Spades";
+  }
+
+  if(cardName === "12 of Clubs") {    
+    cardName = "Queen Of Clubs";
+  }
+  else if(cardName == "12 of Diamonds") {
+    cardName = "Queen of Diamonds";
+  }
+  else if(cardName == "12 of Hearts") {
+    cardName = "Queen of Hearts";
+  }
+  else if(cardName == "12 of Spades") {
+    cardName = "Queen of Spades";
+  }
   
-  alert("You discarded a card.");
+  if(cardName === "13 of Clubs") {    
+    cardName = "King Of Clubs";
+  }
+  else if(cardName == "13 of Diamonds") {
+    cardName = "King of Diamonds";
+  }
+  else if(cardName == "13 of Hearts") {
+    cardName = "King of Hearts";
+  }
+  else if(cardName == "13 of Spades") {
+    cardName = "King of Spades";
+  }
+  
+  alert("You discarded card " + cardName);
   document.getElementById("discardPile").innerHTML = ''
   }
 
@@ -36,7 +89,8 @@ function allowDiscard(ev) {
       adiv.setAttribute("alt", "alt");
       adiv.setAttribute("draggable", "true");
       adiv.setAttribute("ondragstart", "drag(event)");
-      adiv.setAttribute("id", cardCode[x] + i);	
+      //adiv.setAttribute("id", cardCode[x] + i);	
+      adiv.setAttribute("id", i + " of " + cardCode[x] + "s");
       document.getElementById("cardArea").appendChild(adiv);
     }
   }
